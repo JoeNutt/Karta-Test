bc8376f3dd66188c942ae9809ae15734ccf4cf64
Joseph Nutt <JoeNutt01@gmail.com>
2022-10-16 22:12:05 +0100

    Add product class

---
+++ cart/Product.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cart
+{
+     public class Product {
+        public String name { get; set; }
+        public double price { get; set; }
+
+        public Product() {
+            
+        }
+}
+        
+}
